7b8cd6109813b411771c203b327ffd8756083e0d
smasher164 <aindurti@gmail.com>
2020-12-26 19:53:18 -0500

    Squashed 'nixos-docker-sd-image-builder/' content from commit 2a4ec5a
    git-subtree-dir: nixos-docker-sd-image-builder
    git-subtree-split: 2a4ec5a710f79d7a27a6fbe94b6753bb80ba20c9

---
+++ config/sd-image.nix
@@ -0,0 +1,43 @@
+{ lib, ... }: {
+  imports = [
+    ## Uncomment at most one of the following to select the target system:
+    # ./generic-aarch64 # (note: this is the same as 'rpi3')
+    # ./rpi4
+    ./rpi3
+  ];
+
+  # The installer starts with a "nixos" user to allow installation, so add the SSH key to
+  # that user. Note that the key is, at the time of writing, put in `/etc/ssh/authorized_keys.d`
+  users.extraUsers.nixos.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 ..."
+  ];
+
+  # bzip2 compression takes loads of time with emulation, skip it. Enable this if you're low
+  # on space.
+  sdImage.compressImage = false;
+
+  # OpenSSH is forced to have an empty `wantedBy` on the installer system[1], this won't allow it
+  # to be automatically started. Override it with the normal value.
+  # [1] https://github.com/NixOS/nixpkgs/blob/9e5aa25/nixos/modules/profiles/installation-device.nix#L76
+  systemd.services.sshd.wantedBy = lib.mkOverride 40 [ "multi-user.target" ];
+
+  # Enable OpenSSH out of the box.
+  services.sshd.enable = true;
+
+  # Wireless networking (1). You might want to enable this if your Pi is not attached via Ethernet.
+  #networking.wireless = {
+  #  enable = true;
+  #  interfaces = [ "wlan0" ];
+  #  networks = {
+  #    "SSID" = {
+  #      psk = "password";
+  #    };
+  #  };
+  #};
+
+  # Wireless networking (2). Enables `wpa_supplicant` on boot.
+  #systemd.services.wpa_supplicant.wantedBy = lib.mkOverride 10 [ "default.target" ];
+
+  # NTP time sync.
+  #services.timesyncd.enable = true;
+}
---
+++ docker/setup-qemu/build/setup-image.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -eu
+
+if [ -z "${QEMU_PKG_URL+x}" ] || [ -z "${QEMU_PKG_HASH+x}" ]; then
+    echo "determining version of QEMU to use..."
+    readonly DEBIAN_PKG_WEB_PAGE="https://packages.debian.org/unstable/amd64/qemu-user-static/download"
+    curl -o /tmp/deb_pkg_page "$DEBIAN_PKG_WEB_PAGE"
+    if ! QEMU_PKG_URL="$(grep -Eo "https?://ftp.debian.org.*_amd64\.deb" /tmp/deb_pkg_page)"; then
+        echo "unable to retrieve QEMU version from $DEBIAN_PKG_WEB_PAGE, please raise an issue" >&2
+        exit 1
+    fi
+    if ! QEMU_PKG_HASH="$(grep SHA256 /tmp/deb_pkg_page | grep -o '<tt>.*</tt>' | sed 's/[^a-fA-F0-9]//g')"; then
+        echo "unable to retrieve QEMU package hash from $DEBIAN_PKG_WEB_PAGE, please raise an issue" >&2
+        exit 1
+    fi
+    echo "QEMU package URL: $QEMU_PKG_URL"
+    echo "QEMU package hash: $QEMU_PKG_HASH"
+    rm /tmp/deb_pkg_page
+fi
+
+wget "$QEMU_PKG_URL"
+
+QEMU_PKG=qemu*.deb
+
+echo "verifying hash of" $QEMU_PKG "..."
+# note: spaces matter here!
+echo "$QEMU_PKG_HASH " $QEMU_PKG > sha256
+
+sha256sum -c sha256 || exit 1
+
+echo "extracting..."
+# avoid extracting the whole thing (which is pretty big uncompressed), only retrieve the AArch64
+# binary.
+dpkg --fsys-tarfile $QEMU_PKG | tar xvF - ./usr/bin/qemu-aarch64-static --strip=3
+
+echo "image is ready"
